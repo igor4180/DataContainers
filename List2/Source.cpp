4c679924f1380ed5e955f7267b84885b18e51d6c
Игорь <@DESKTOP-B894K3L>
2022-08-09 23:07:32 +0300

    InheritIteratorsInList2

--- List2/Source.cpp
+++ List2/Source.cpp
@@ -1,4 +1,5 @@
 ﻿#include<iostream>
+#include<exception>
 using namespace std;
 using std::cin;
 using std::cout;
@@ -29,12 +30,175 @@ class List
 	//Element* Head;
 	//Element* Tail;
 	unsigned int size;
+	class ConstBaseIterator
+	{
+	protected:
+		Element* Temp;
+	public:
+		ConstBaseIterator(Element* Temp = nullptr) :Temp(Temp)
+		{
+			cout << "CBItConstructor:\t" << this << endl;
+		}
+		~ConstBaseIterator()
+		{
+			cout << "CBItDestructor:\t" << this << endl;
+		}
+
+		bool operator==(const ConstBaseIterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator!=(const ConstBaseIterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+
+		const int& operator*()const
+		{
+			return Temp->Data;
+		}
+	};
 public:
+	class ConstIterator :public ConstBaseIterator
+	{
+	public:
+		ConstIterator(Element* Temp) :ConstBaseIterator(Temp)
+		{
+#ifdef DEBUG
+			cout << "CItConstructor:\t" << this << endl;
+#endif // DEBUG
+		}
+		~ConstIterator()
+		{
+#ifdef DEBUG
+			cout << "CItDestructor:\t" << this << endl;
+#endif // DEBUG
+		}
+
+		ConstIterator& operator++()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		ConstIterator operator++(int)
+		{
+			ConstIterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+		ConstIterator& operator--()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		ConstIterator operator--(int)
+		{
+			ConstIterator old = *this;
+			Temp = Temp->pPrev;
+			return old;
+		}
+	};
+	class ConstReverseIterator :public ConstBaseIterator
+	{
+	public:
+		ConstReverseIterator(Element* Temp) :ConstBaseIterator(Temp)
+		{
+#ifdef DEBUG
+			cout << "CRItConstructor:\t" << this << endl;
+#endif // DEBUG
+		}
+		~ConstReverseIterator()
+		{
+#ifdef DEBUG
+			cout << "CRItDestructor:\t" << this << endl;
+#endif // DEBUG
+		}
+
+		ConstReverseIterator& operator++()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+		ConstReverseIterator operator++(int)
+		{
+			ConstReverseIterator old = *this;
+			Temp = Temp->pPrev;
+			return old;
+		}
+		ConstReverseIterator& operator--()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		ConstReverseIterator operator--(int)
+		{
+			ConstReverseIterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+	};
+	class Iterator :public ConstIterator
+	{
+	public:
+		Iterator(Element* Temp = nullptr) :ConstIterator(Temp) {}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+	};
+	class ReverseIterator :public ConstReverseIterator
+	{
+	public:
+		ReverseIterator(Element* Temp = nullptr) :ConstReverseIterator(Temp) {}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+	};
+
+	ConstIterator cbegin()const
+	{
+		return Head;
+	}
+	ConstIterator cend()const
+	{
+		return nullptr;
+	}
+	ConstReverseIterator crbegin()
+	{
+		return Tail;
+	}
+	ConstReverseIterator crend()
+	{
+		return nullptr;
+	}
+	Iterator begin()
+	{
+		return Head;
+	}
+	Iterator end()
+	{
+		return nullptr;
+	}
+
 	List()
 	{
 		Head = Tail = nullptr;//Когда список пуст, его Голова и Хвост указывают на 0
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
+	}
+	List(const std::initializer_list<int>& il) :List()
+	{
+		//int* - указатель
+		//const int* - константный указатель
+		//int const* - указатель на константу
+		//const int const* - константный указатель на константу
+		for (int const* it = il.begin(); it != il.end(); ++it)
+			push_back(*it);
+	}
+	List(const List& other) :List()
+	{
+		for (ConstIterator it = other.cbegin(); it != other.cend(); ++it)push_back(*it);
 	}
 	~List()
 	{
@@ -86,7 +250,9 @@ public:
 	}
 	void insert(int Data, int index)
 	{
-		if (index > size)return;
+		if (index > size)throw std::out_of_range("Error: Out of range exception in insert() function");
+		//throw std::exception("Error: Out of range");
+	//Out of range - выход за пределы
 		if (index == 0)return push_front(Data);
 		if (index == size)return push_back(Data);
 
@@ -137,7 +303,7 @@ public:
 	}
 	void erase(int index)
 	{
-		if (index >= size)return;
+		if (index >= size)throw std::out_of_range("Error: Out of range exception in erase() function");
 		if (index == 0)return pop_front();
 		//1) Доходим до удаляемого элемента:
 		Element* Temp;
@@ -176,9 +342,25 @@ public:
 	}
 };
 
+List operator+(const List& left, const List& right)
+{
+	List cat = left;
+	for (List::ConstIterator it = right.cbegin(); it != right.cend(); ++it)
+	{
+		cat.push_back(*it);
+		//(*it) *= 100;
+	}
+	return cat;
+}
+
+//#define BASE_CHECK
+//#define ITERATORS_CHECK_1
+#define ITERATORS_CHECK_2
+
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef BASE_CHECK
 	int n;
 	cout << "Введите размер списка: "; cin >> n;
 	List list;
@@ -194,11 +376,48 @@ void main()
 	int index;
 	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	cout << "Введите индекс добавляемого элемента: "; cin >> index;
-	list.insert(value, index);
-	list.print();
-	list.reverse_print();
+	try
+	{
+		list.insert(value, index);
+		list.print();
+		list.reverse_print();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << endl;
+	}
 	cout << "Введите индекс удаляемого элемента: "; cin >> index;
-	list.erase(index);
+	try
+	{
+		list.erase(index);
+		list.print();
+		list.reverse_print();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << endl;
+	}
+#endif // BASE_CHECK
+
+#ifdef ITERATORS_CHECK_1
+	List list = { 3,5,8,13,21 };
 	list.print();
-	list.reverse_print();
+	for (int i : list)
+		cout << i << tab;
+	cout << endl;
+
+	List::ReverseIterator rend = list.rend();
+	for (List::ReverseIterator rit = list.rbegin(); rit != rend; ++rit)
+	{
+		cout << *rit << tab;
+	}
+	cout << endl;
+#endif // ITERATORS_CHECK_1
+
+	List list1 = { 3,5,8,13,21 };
+	List list2 = { 34,55,89 };
+	List list3 = list1 + list2;
+	for (int i : list1)cout << i << tab; cout << endl;
+	for (int i : list2)cout << i << tab; cout << endl;
+	for (int i : list3)cout << i << tab; cout << endl;
 }
